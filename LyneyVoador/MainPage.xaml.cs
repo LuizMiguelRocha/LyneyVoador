61bcdadc2a8d2f666986e742ef260f171f3c51be
Luiz Miguel <luizmiguel.ramos.escola@gmail.com>
2024-10-15 08:53:49 -0300

    .

--- LyneyVoador/MainPage.xaml.cs
+++ LyneyVoador/MainPage.xaml.cs
@@ -6,5 +6,51 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	bool VerificaColisao()
+	{
+		if(!estaMorto)
+		{
+			if(VerificaColisaoTeto() || VerificaColisaoChao())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool VerificaColisaoTeto()
+	{
+		var minY = -alturaJanela/2;
+		if(imglyney.TranslationY <= minY)
+		return true;
+		else
+		return false;
+	}
+
+	bool VerificaColisaoChao()
+	{
+		var maxY = alturaJanela/2 - imgChao.HeightRequest;
+		if (imglyney.TranslationY >= maxY)
+		return true;
+		else
+		return false;
+	}
+
+	async Task Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			GerenciaCanos();
+			if(VerificaColisao())
+			{
+				estaMorto=true;
+				frameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(TempoEntreFrames);
+		}
+	}
 }
 
