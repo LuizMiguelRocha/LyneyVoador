4c81367ec3a2a62edd35140b286e200f6289a28e
Luiz Miguel <luizmiguel.ramos.escola@gmail.com>
2024-10-23 09:44:20 -0300

    .

--- LyneyVoador/MainPage.xaml.cs
+++ LyneyVoador/MainPage.xaml.cs
@@ -2,25 +2,25 @@
 
 public partial class MainPage : ContentPage
 {
-	const int gravidade = 2;
+	const int gravidade = 5;
 	const int TempoEntreFrames = 25;
 	bool estaMorto = false;
 	double larguraJanela = 0;
 	double alturaJanela = 0;
-	int velocidade = 20;
+	int velocidade = 5;
 	const int ForcaPulo = 30;
 	const int MaxTempoPulando = 3;
 	bool EstaPulando = false;
 	int TempoPulando = 0;
-	const int AberturaMinima = 30;
+	const int AberturaMinima = 200;
 	int score = 0;
 
 	void Inicializar()
 	{
 		imglyney.TranslationX = 0;
-		imglyney.TranslationX = 0;
+		imglyney.TranslationY = 0;
 		imgcanocima.TranslationX = -larguraJanela;
-		imgcanobaixo.TranslationY = -larguraJanela;
+		imgcanobaixo.TranslationX = -larguraJanela;
 		score = 0;
 		GerenciaCanos();
 	}
@@ -71,19 +71,21 @@ public partial class MainPage : ContentPage
 	{
 		while (!estaMorto)
 		{
-			AplicaGravidade();
+			if (EstaPulando)
+				AplicaPulo();
+			else
+				AplicaGravidade();
+
 			GerenciaCanos();
+
 			if (VerificaColisao())
 			{
 				estaMorto = true;
 				FrameGameOver.IsVisible = true;
 				break;
 			}
+
 			await Task.Delay(TempoEntreFrames);
-			if (EstaPulando)
-				AplicaPulo();
-			else
-				AplicaGravidade();
 		}
 	}
 
@@ -95,59 +97,57 @@ public partial class MainPage : ContentPage
 		{
 			imgcanobaixo.TranslationX = 0;
 			imgcanocima.TranslationX = 0;
-			var alturaMax = -100;
+			var alturaMax = -300;
 			var alturaMin = -imgcanobaixo.HeightRequest;
 			imgcanocima.TranslationY = Random.Shared.Next((int)alturaMin, (int)alturaMax);
 			imgcanobaixo.TranslationY = imgcanocima.HeightRequest + imgcanocima.TranslationY + AberturaMinima;
 			score++;
+			if (score % 2 == 0)
+				velocidade++;
 			labelScore.Text = "Cano: " + score.ToString("D3");
-			fianlScore.Text = "Total de canos passados  " + score.ToString("");
+			fianlScore.Text = "Total de canos passados  " + score.ToString("D3");
 		}
 	}
 
 
 	bool VerificaColisao()
 	{
+		return VerificaColisaoTeto() ||
+			VerificaColisaoChao() ||
+			VerificaColisaoCanoCima() ||
+			VerificaColisaoCanoBaixo();
+	}
 
-		if (!estaMorto)
+	bool VerificaColisaoCanoCima()
+	{
+		var posHimglyney = (larguraJanela / 2) - (imglyney.WidthRequest / 2);
+		var posVimglyney = (alturaJanela / 2) - (imglyney.HeightRequest / 2) + imglyney.TranslationY;
+		if (posHimglyney >= Math.Abs(imgcanocima.TranslationX) - imgcanocima.WidthRequest &&
+		   posHimglyney <= Math.Abs(imgcanocima.TranslationX) + imgcanocima.WidthRequest &&
+		   posVimglyney <= imgcanocima.HeightRequest + imgcanocima.TranslationY)
 		{
-			if (VerificaColisaoTeto() || VerificaColisaoChao() || VerificaColisaoCanoCima() || VerificaColisaoCanoBaixo())
-			{
-				return true;
-			}
+			return true;
 		}
-		return false;
-
-			bool VerificaColisaoCanoCima()
+		else
 		{
-			var posHimglyney = (larguraJanela / 2) - (imglyney.WidthRequest / 2);
-			var posVimglyney = (alturaJanela / 2) - (imglyney.HeightRequest / 2) + imglyney.TranslationY;
-			if(posHimglyney >= Math.Abs(imgcanocima.TranslationX) - imgcanocima.WidthRequest&&
-			   posHimglyney <= Math.Abs(imgcanocima.TranslationX) + imgcanocima.WidthRequest&&
-			   posVimglyney <= imgcanocima.HeightRequest + imgcanocima.TranslationY)
-			   {
-				return true;
-			   }
-			   else
-			   {
-				return false;
-			   }
+			return false;
 		}
+	}
 
-		bool VerificaColisaoCanoBaixo()
+	bool VerificaColisaoCanoBaixo()
+	{
+		var posHimglyney = (larguraJanela / 2) - (imglyney.WidthRequest / 2);
+		var posVimglyney = (alturaJanela / 2) + (imglyney.HeightRequest / 2) + imglyney.TranslationY;
+		var yMaxCano = imgcanocima.HeightRequest + imgcanocima.TranslationY + AberturaMinima;
+		if (posHimglyney >= Math.Abs(imgcanobaixo.TranslationX) - imgcanobaixo.WidthRequest &&
+		   posHimglyney <= Math.Abs(imgcanobaixo.TranslationX) + imgcanobaixo.WidthRequest &&
+		   posVimglyney >= yMaxCano)
 		{
-			var posHimglyney = (larguraJanela / 2) - (imglyney.WidthRequest / 2);
-			var posVimglyney = (alturaJanela / 2) - (imglyney.HeightRequest / 2) + imglyney.TranslationY;
-			if(posHimglyney >= Math.Abs(imgcanobaixo.TranslationX) - imgcanobaixo.WidthRequest&&
-			   posHimglyney <= Math.Abs(imgcanobaixo.TranslationX) + imgcanobaixo.WidthRequest&&
-			   posVimglyney <= imgcanobaixo.HeightRequest + imgcanobaixo.TranslationY)
-			   {
-				return true;
-			   }
-			   else
-			   {
-				return false;
-			   }
+			return true;
+		}
+		else
+		{
+			return false;
 		}
 	}
 
